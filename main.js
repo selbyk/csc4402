a1a87473a201a0186d929ebb8d2e9e9fbb0f8666
Selby Kendrick <selby.kendrick@gmail.com>
2014-12-03 22:45:13 -0600

    Long night, works up to minimal covers

--- main.js
+++ main.js
@@ -31,6 +31,42 @@ Array.prototype.equals = function (array) {
     return true;
 }
 
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.proper_subset = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time
+    if (this.length == 0 || array.length == 0 || this.length < array.length)
+        return false;
+
+    for (var i = 0, l=array.length; i < l; i++) {
+      if(this.indexOf(array[i]) == -1) {
+        return false;
+      }
+    }
+    return true;
+}
+
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.proper_subset = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time
+    if (this.length == 0 || array.length == 0 || this.length <= array.length)
+        return false;
+
+    for (var i = 0, l=array.length; i < l; i++) {
+      if(this.indexOf(array[i]) == -1) {
+        return false;
+      }
+    }
+    return true;
+}
+
 /*
 Your previous Plain Text content is preserved below:
 
@@ -141,6 +177,7 @@ var fd_closure = function(a_attrs, kva_fds){
 }
 
 /*
+From course:
 MINIMAL COVERS
 Let F 1 and F 2 be two sets of functional dependencies.
 If F1 ≡ F2,then we say the F1 is a cover ofF2 and F2 is a cover of
@@ -162,33 +199,103 @@ var break_rhs = function(kva_fds){
   var new_fds = [];
   kva_fds.forEach(function(fd){
     fd[1].forEach(function(attr){
-      new_fds.push([fd[0],attr]);
+      new_fds.push([fd[0],[attr]]);
     });
   });
   return new_fds;
 }
 
 //
-/*Eliminate redundancy in the left hand side.The fd CD→Aisreplaced by C
-→A. This is becauseC→D∈(F′)+,hence C→CD∈(F′)+;from C→CD∈(F′)+and CD→
-A∈F′,bytransitivity,wehav eC→A∈(F′)+and henceCD→Ashould be replaced by C→
-A. Similarly,CD→Bis replaced by C→B, AE→Cisreplaced by A→C. F′=
-{C→A, C→B, C→D, D→E, D→H, A→C, B→D}after step (2).
+/*
+From course:
+Eliminate redundancy in the left hand side.The fd CD→A is replaced by C
+→A. This is because C→D ∈ (F′)+, hence C→CD ∈ (F′)+; from C→CD ∈(F′)+ and CD→A ∈ F′,
+by transitivity, we have C→A∈(F′)+ and hence CD→A should be replaced by C→A. Similarly,
+CD→B is replaced by C→B, AE→C is replaced by A→C. F′= {C→A, C→B, C→D, D→E, D→H, A→C, B→D}
+after step (2).
 */
-var elim_lhs_redundancy = function(kva_fds){
-  fm = break_rhs(kva_fds);
-
+var elim_lhs_redundancy = function(a_keys, kva_fds){
+  var fm = break_rhs(kva_fds);
+  do{
+    var contin = false;
+    var new_fm = [];
+    var fmc = fd_closure(a_keys, fm);
+    fm.forEach(function(fd){
+      if(fd[0].length > 1){ // it is a composite key
+        // CSC 4402 Notes
+        // X is a composite attribute and
+        // Z ⊂ X is a proper subset of X and Z → A ∈ (F′)+, do replace X → A
+        // with Z → A.
+        var replaced = false;
+        for(i=0;i<fmc.length;++i){
+          var is_proper_subset = fd[0].proper_subset(fmc[i][0]);
+          //console.log(fd[0] + '->' + fd[1] + ':' + fmc[i][0] + '->' + fmc[i][1]);
+          //console.log(is_proper_subset);
+          if(is_proper_subset && fmc[i][1].indexOf(fd[1][0]) != -1){
+            replaced = true;
+            var already_included = false;
+            for(j = 0; j < fm.length; ++j)
+              if(fm[j].equals([fmc[i][0],fd[1][0]])){
+                already_included = true;
+                break;
+              }
+            if(already_included == false)
+              new_fm.push([fmc[i][0],fd[1]]);
+            break;
+          }
+        }
+        if(replaced == false)
+          new_fm.push([fd[0],fd[1]]);
+      }else{
+        new_fm.push([fd[0],fd[1]]);
+      }
+    })
+    if(fm.equals(new_fm) === false){
+      fm = new_fm;
+      contin = true;
+    }
+  }while(contin);
   return fm;
 }
 
-var minimal_cover = function(kva_fds){
-  var left_hand_sides = combinations(a_attrs);
-  var closure = [];
-  left_hand_sides.forEach(function(lhs){
-    var closure_of_x = [lhs, closure_of(lhs, kva_fds)];
-    closure.push(closure_of_x);
-  });
-  return closure;
+var remove_redundant_fds = function(a_keys, kva_fds){
+  // F′= {C→A, C→B, C→D, D→E, D→H, A→C, B→D}
+  // CSC 4402 Notes
+  // The fd C→D is eliminated because it can be derived from C→B and B→D and hence it is
+  // redundant. The F′now becomes {C→A, C→B, D→E, D→H, A→C, B→D}, which is the only
+  // minimal cover of F.♣
+  var fm = elim_lhs_redundancy(a_keys, kva_fds),
+      new_fm = [],
+      X = [],
+      coX = [],
+      attrs = []
+
+  fm.forEach(function(fd){
+    if(fd[0].equals(X) === false){
+      X = fd[0]
+      attrs = []
+    }
+    console.log(attrs);
+    var redundant = false;
+    for(i=0;i<attrs.length;++i){
+      for(j=0;j<fm.length;++j){
+        if(fm[j][0].indexOf(attrs[i]) > -1 && fm[j][1].indexOf(fd[1][0]) > -1)
+          redundant = true;
+      }
+      if(redundant)
+        break;
+    }
+    if(redundant === false){
+      new_fm.push([fd[0],fd[1]])
+      attrs.push(fd[1][0])
+    }
+  })
+
+  return new_fm
+}
+
+var minimal_cover = function(a_keys, kva_fds){
+  return remove_redundant_fds(a_keys, kva_fds)
 }
 
 /*
@@ -280,15 +387,47 @@ var expected_break = [
   [['B'], ['D']]
 ];
 
+// F′= {C→A, C→B, C→D, D→E, D→H, A→C, B→D} after step (2).
+var expected_elim = [
+  [['C'], ['A']],
+  [['C'], ['B']],
+  [['C'], ['D']],
+  [['D'], ['E']],
+  [['D'], ['H']],
+  [['A'], ['C']],
+  [['B'], ['D']]
+];
+
+// F′now becomes {C→A, C→B, D→E, D→H, A→C, B→D}
+var expected_min_cover = [
+  [['C'], ['A']],
+  [['C'], ['B']],
+  [['D'], ['E']],
+  [['D'], ['H']],
+  [['A'], ['C']],
+  [['B'], ['D']]
+];
+
 console.log('X:');
 console.log(X);
 console.log('F:');
 console.log(F);
 var break_calculated = break_rhs(F);
 console.log('Testing min-cover part 1...');
-console.log('F`:');
+console.log('F` (1/3):');
 test_output(break_calculated, expected_break);
 console.log(break_calculated);
+var elim_calculated = elim_lhs_redundancy(X, F);
+console.log('Testing min-cover part 2...');
+console.log('F` (2/3):');
+test_output(elim_calculated, expected_elim);
+console.log(elim_calculated);
+var min_cover_calculated = minimal_cover(X, F);
+console.log('Testing min-cover part 3...');
+console.log('F` (3/3):');
+test_output(min_cover_calculated, expected_min_cover);
+console.log(min_cover_calculated);
+
 
 /*Eliminate redundancy in the left hand side.The fd CD→Aisreplaced by C
 →A. This is becauseC→D∈(F′)+,hence C→CD∈(F′)+;from C→CD∈(F′)+and CD→
